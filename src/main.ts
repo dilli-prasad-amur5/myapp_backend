ecf8b747189e7799c084d308b902f2adff9b6fc9
dilli-prasad-amur5 <dilliprasadamur@gmail.com>
2025-08-02 20:28:29 +0200

    update paths

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ if (!process.env.IS_TO_NODE){
 }
 
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
