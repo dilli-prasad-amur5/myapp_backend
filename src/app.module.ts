777b35ab9cebc96ac97ef642f7a1c528f278aa47
dilli-prasad-amur5 <dilliprasadamur@gmail.com>
2025-08-03 16:44:42 +0200

    added postgres config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TagModule],
+  imports: [TagModule,
+    // Todo: move database confid data to new file
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'testuser1',
+      password: 'password',
+      database: 'mediumclone',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
